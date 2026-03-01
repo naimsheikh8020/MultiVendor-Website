878e090e951e2cd1fda6524f902b2664eb2043a9
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-03-01 11:08:38 +0600

    assets added

---
+++ src/assets/assets.ts
@@ -0,0 +1,23 @@
+import Category from "./Category.svg";
+import logo from "./logo.svg"
+import Popular_Product1 from "./Popular_Product-1.svg"
+import Popular_Product2 from "./Popular_Product-2.svg"
+import Popular_Product3 from "./Popular_Product-3.svg"
+import Popular_Product4 from "./Popular_Product-4.svg"
+import Popular_Product5 from "./Popular_Product-5.svg"
+import Popular_Product6 from "./Popular_Product-6.svg"
+import Popular_Product7 from "./Popular_Product-7.svg"
+import Popular_Product8 from "./Popular_Product-8.svg"
+
+export const assets = {
+  Category,
+  logo,
+  Popular_Product1,
+  Popular_Product2,
+  Popular_Product3,
+  Popular_Product4,
+  Popular_Product5,
+  Popular_Product6,
+  Popular_Product7,
+  Popular_Product8
+}
