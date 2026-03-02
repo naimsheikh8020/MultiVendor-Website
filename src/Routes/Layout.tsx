06e00a4d5e9ea7ff894f4155c5e6d291ebf0c1ca
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-03-02 10:48:52 +0600

    Refactor code structure for improved readability and maintainability

--- src/Routes/Layout.tsx
+++ src/Routes/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden">
       <Navbar />
-      <main className="flex-1 px-4 md:px-8 py-6 overflow-x-hidden">
+      <main className="flex-1 px-4 md:px-8 overflow-x-hidden">
         <Outlet />
       </main>
       <Footer />
--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -8,9 +8,14 @@ import Popular_Product5 from "./Popular_Product-5.svg"
 import Popular_Product6 from "./Popular_Product-6.svg"
 import Popular_Product7 from "./Popular_Product-7.svg"
 import Popular_Product8 from "./Popular_Product-8.svg"
+import Hero_headPhone from "./Hero_headPhone.svg"
+import Hero_laptop from "./Hero_laptop.svg"
+import Hero_phone from "./hero_phone.png"
+import hero_vegetable from "./Hero_vegetable-removebg-preview.png"
 
 export const assets = {
   Category,
+  hero_vegetable,
   logo,
   Popular_Product1,
   Popular_Product2,
@@ -19,7 +24,10 @@ export const assets = {
   Popular_Product5,
   Popular_Product6,
   Popular_Product7,
-  Popular_Product8
+  Popular_Product8,
+  Hero_headPhone,
+  Hero_laptop,
+  Hero_phone,
 }
 
 export const categoryNames = [
