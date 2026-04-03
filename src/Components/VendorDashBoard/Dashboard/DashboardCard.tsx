3cc9bcb5e5e55292d304e295ed3c0a9840565ad2
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-04-03 18:49:10 +0600

    add DashboardCard component and update VendorDashboard to display vendor metrics

---
+++ src/Components/VendorDashBoard/Dashboard/DashboardCard.tsx
@@ -0,0 +1,35 @@
+type DashboardCardProps = {
+  icon: string;
+  value: string | number;
+  label: string;
+};
+
+const DashboardCard = ({ icon, value, label }: DashboardCardProps) => {
+  return (
+    <div className="bg-white rounded-xl border border-gray-200 p-6 w-full shadow-sm
+                    transition-all duration-300 ease-out
+                    hover:shadow-md hover:-translate-y-1 hover:border-gray-300">
+      
+      {/* Icon */}
+      <div className="w-12 h-12 mb-5 transition-transform duration-300 ease-out group-hover:scale-105">
+        <img
+          src={icon}
+          alt={label}
+          className="w-full h-full object-contain"
+        />
+      </div>
+
+      {/* Value */}
+      <h2 className="text-2xl font-bold text-gray-900 transition-colors duration-300">
+        {value}
+      </h2>
+
+      {/* Label */}
+      <p className="text-sm text-gray-500 mt-2">
+        {label}
+      </p>
+    </div>
+  );
+};
+
+export default DashboardCard;
--- src/features/vendors/VendorDashboard.tsx
+++ src/features/vendors/VendorDashboard.tsx
@@ -1,8 +1,35 @@
+import { assets } from "../../assets/assets"
+import DashboardCard from "../../Components/VendorDashBoard/Dashboard/DashboardCard"
+
 const VendorDashboard = () => {
   return (
     <div>
       <h1 className="font-bold text-xl text-gray-600 mb-2">Dashboard</h1>
       <p className="text-gray-600 ">Welcome back, <span className="font-semibold text-blue-600">Sohan Smith</span>!</p>
+
+      <div className="flex gap-4 w-full mt-6">
+        <DashboardCard
+          icon={assets.VendorTotalSales}
+          value="৳ 45600.00"
+          label="Total Sales"
+        />
+        <DashboardCard
+          icon={assets.VendorNetEarning}
+          value={"৳ 45600.00"}
+          label="Net Earnings"
+        />
+        <DashboardCard
+          icon={assets.VendorTotalProduct}
+          value="5"
+          label="Total Products"
+        />
+        <DashboardCard
+          icon={assets.VendorTotalOrder}
+          value="1000"
+          label="Total Orders"
+        />
+
+      </div>
     </div>
   )
 }
