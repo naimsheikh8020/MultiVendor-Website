11a6d155bbd515a05ae81ec7f385c106da76455c
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-04-11 15:31:11 +0600

    add search bar filter

--- src/features/Admin/AdminRecommendedStore.tsx
+++ src/features/Admin/AdminRecommendedStore.tsx
@@ -1,8 +1,24 @@
+import FilterBar from "../../Components/FilterBar"
 
 const AdminRecommendedStore = () => {
   return (
-    <div>
-      AdminRecommendedStore
+    <div className="space-y-4">
+      {/* Header */}
+      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <div>
+          <h1 className="mb-1 text-lg sm:text-xl font-bold text-gray-700">
+            Recommended stores
+          </h1>
+          <p className="text-sm sm:text-base text-gray-600">
+            Track and manage recommended stores
+          </p>
+        </div>
+      </div>
+
+       {/* SEARCH + FILTER */}
+        <FilterBar showSearch/>
+
+        {/* Recommended Stores List */}
     </div>
   )
 }
