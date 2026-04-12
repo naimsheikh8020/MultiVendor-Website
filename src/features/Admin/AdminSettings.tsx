f7680bbaa61fbfe0dedf5fcfc5d4200582d5f95d
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-04-12 10:36:05 +0600

    admin setting header done

--- src/features/Admin/AdminSettings.tsx
+++ src/features/Admin/AdminSettings.tsx
@@ -1,8 +1,18 @@
 
 const AdminSettings = () => {
   return (
-    <div>
-      AdminSettings
+    <div className="space-y-4">
+      {/* Header */}
+      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <div>
+          <h1 className="mb-1 text-lg sm:text-xl font-bold text-gray-700">
+            Settings
+          </h1>
+          <p className="text-sm sm:text-base text-gray-600">
+            Manage platform settings and preferences
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
