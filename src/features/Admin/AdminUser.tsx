a2379aa774a397993dddedc708121b07e1624ca1
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-04-12 16:20:15 +0600

    user header added

--- src/features/Admin/AdminUser.tsx
+++ src/features/Admin/AdminUser.tsx
@@ -1,8 +1,18 @@
 
 const AdminUser = () => {
   return (
-    <div>
-      AdminUser
+    <div className="space-y-4">
+      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <div>
+          <h1 className="mb-1 text-lg sm:text-xl font-bold text-gray-700">
+            User Management
+          </h1>
+          <p className="text-sm sm:text-base text-gray-600">
+           Manage vendors and customers
+          </p>
+        </div>
+      </div>
+      
     </div>
   )
 }
