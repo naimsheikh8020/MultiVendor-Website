e4bac81813f366c20bc0f142bf5916758b0d45c0
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-04-25 14:47:43 +0600

    fix the issue

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   ShoppingCart,
-  Heart,
+ 
   Search,
   ChevronDown,
   User,
