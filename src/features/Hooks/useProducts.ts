b043f5af80bdeb5f98efb5dfd0c903d056235337
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-05-05 13:20:08 +0600

    optimize the product code

--- src/features/Hooks/useProducts.ts
+++ src/features/Hooks/useProducts.ts
@@ -5,4 +5,8 @@ export const useProducts = () =>
   useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
+    staleTime: 1000 * 60 * 5, // ✅ 5 min cache (important)
+    gcTime: 1000 * 60 * 30,   // ✅ keep in memory 30 min
+
+    refetchOnWindowFocus: false, // ✅ avoid annoying refetch
   });
